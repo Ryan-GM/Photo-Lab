66d1406acd6ee34b1abb7aab955110cb0a113e0a
Munge Ryan <mungeryan@gmail.com>
2020-11-15 17:31:01 +0300

    changing from path to url

--- photos/urls.py
+++ photos/urls.py
@@ -1,16 +1,14 @@
-from django.urls import path
-from django.urls.conf import re_path
+from django.conf.urls import url
 from . import views
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'photos'
 
 urlpatterns = [
-    path(r'^$', views.index, name='index'),
-    path(r'^search/', views.search_results, name='search'),
-    re_path(r'^location/(?P<location>\w+)/', views.photo_location, name='location'),
+    url(r'^$', views.index, name='index'),
+    url(r'^search/', views.search_results, name='search'),
+    url(r'^location/(?P<location>\w+)/', views.photo_location, name='location'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    
