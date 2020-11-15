648705433a96c8720c35c2fa4f056cec09b7576e
Munge Ryan <mungeryan@gmail.com>
2020-11-15 16:58:02 +0300

    defining a teardown

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,50 @@
 from django.test import TestCase
-
+from .models import Photo,Location,Category
 # Create your tests here.
+
+class TestPhoto(TestCase):
+    def setUp(self):
+        self.location = Location(name="Strath")
+        self.location.save_location()
+        self.category = Category(name='Bike')
+        self.category.save_category()
+        self.photo_test = Photo(id = 1,name = 'photo',description = 'to test the photo',location = self.location,category = self.category)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.photo_test, Photo))
+
+    def test_save_photo(self):
+        self.image_test.save_photo()
+        after = Photo.objects.all()
+        self.assertTrue(len(after) > 0)
+
+    def test_delete_photo(self):
+        self.photo_test.delete_image()
+        photos = Photo.objects.all()
+        self.assertTrue(len(photos) == 0)
+
+    def test_update_photo(self):
+        self.photo_test.save_photo()
+        self.photo_test.update_photo(self.photo_test.id, 'photos/test.jpg')
+        changed_img = Photo.objects.filter(photo='photos/test.jpg')
+        self.assertTrue(len(changed_img) > 0)
+
+    def test_get_photo_by_id(self):
+        found_photo = self.photo_test.get_photo_by_id(self.photo_test.id)
+        photo = Photo.objects.filter(id=self.image_test.id)
+        self.assertTrue(found_photo, photo)
+
+    def test_search_photo_by_location(self):
+        self.image_test.save_image()
+        found_photo = self.photo_test.filter_by_location(location='Strath')
+        self.assertTrue(len(found_photo) == 1)
+
+    def test_search_photo_by_category(self):
+        category = 'Bike'
+        found_photo = self.photo_test.search_by_category(category)
+        self.assertTrue(len(found_photo) > 1)
+
+    def tearDown(self):
+        Photo.objects.all().delete()
+        Location.objects.all().delete()
+        Category.objects.all().delete()
