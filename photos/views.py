5acd267d177be1517e0e8590d1e776b4e3f87a40
Munge Ryan <mungeryan@gmail.com>
2020-11-15 22:22:59 +0300

    importing forms

--- photos/views.py
+++ photos/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Location,Photo
+from .forms import PhotoForm
+from cloudinary.forms import cl_init_js_callbacks
+
 # Create your views here.
 def photo_location(request, location):
     photos = Photo.filter_by_location(location)
@@ -7,10 +10,11 @@ def photo_location(request, location):
     return render(request,'photos/locale.html',{'location_photos':photos})
 
 def index(request):
+    form = PhotoForm()
     photos = Photo.objects.all()
     locations = Location.get_locations()
     print(locations)
-    return render(request, 'photos/index.html', {'photos':photos[::-1],'locations': locations})
+    return render(request, 'photos/index.html', {'photos':photos[::-1],'locations': locations,'form': form})
 
 def search_results(request):
     if 'photosearch' in request.GET and request.GET["photosearch"]:
