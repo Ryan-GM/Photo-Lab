7bb95f9739d9d32181f0adb119219402059755c8
Munge Ryan <mungeryan@gmail.com>
2020-11-15 18:01:10 +0300

    defining bootstrap4 connecting to django-bootstrap

--- lab/settings.py
+++ lab/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'photos'
+    'photos',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
