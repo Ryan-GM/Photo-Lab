2a0ebddf80f41c430cfe143f161e6085fd8d0f62
Munge Ryan <mungeryan@gmail.com>
2020-11-15 15:34:47 +0300

    creating class for photos

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Photos(models.Mode):
+    photo = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=60)
+    description = models.TextField()
+    author = models.CharField(max_length=40, default='admin')
+    date = models.DateTimeField(auto_now_add=True)
+    type = models.ForeignKey(Type)
+    location = models.ForeignKey(Location)
+
+    
