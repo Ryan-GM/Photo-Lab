1716617024bfcff06dc231e20fe665294dcc7d8b
Munge Ryan <mungeryan@gmail.com>
2020-11-15 15:51:18 +0300

    defining class method for updating location

--- photos/models.py
+++ photos/models.py
@@ -30,4 +30,47 @@ class Photos(models.Mode):
         photo = cls.objects.filter(type__name__icontains=type)
         return photo
 
-    
+    def __str__(self):
+        return self.name
+
+    def save_photo(self):
+        self.save()
+
+    def delete_photo(self):
+        self.delete()
+
+    class Meta:
+        ordering = ['date']
+
+class Type(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    def save_type(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+class Location(models.Model):
+    name = models.CharField(max_length=60)
+
+    @classmethod
+    def get_locations(cls):
+        locations = Location.objects.all()
+        return locations
+
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete() 
