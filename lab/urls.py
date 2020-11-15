ef6f7a2fb0f9149020c4427e8b086d6095473f20
Munge Ryan <mungeryan@gmail.com>
2020-11-15 17:38:59 +0300

    changing from path to url

--- lab/urls.py
+++ lab/urls.py
@@ -13,11 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import include
 
 urlpatterns = [
-    path(r'^admin/', admin.site.urls),
-    path(r'', include('photos.urls'))
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('photos.urls')),
 ]
