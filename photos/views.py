0cbd21ac8e8b4e4bd4cec3a29de16fb28d118086
Munge Ryan <mungeryan@gmail.com>
2020-11-15 16:24:28 +0300

    rendering photo location to locale.html

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import Location,Photo
 # Create your views here.
+def photo_location(request, location):
+    photos = Photo.filter_by_location(location)
+    print(photos)
+    return render(request,'photos/locale.html',{'location_photos':photos})
+
+def index(request):
+    photos = Photo.objects.all()
+    locations = Location.get_locations()
+    print(locations)
+    return render(request, 'photos/index.html', {'photos':photos[::-1],'locations': locations})
