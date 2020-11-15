7943ca3b756076c8839f0ab069a114ea186cf11c
Munge Ryan <mungeryan@gmail.com>
2020-11-15 16:08:30 +0300

    defining a search method by type

--- photos/models.py
+++ photos/models.py
@@ -2,57 +2,18 @@ from django.db import models
 
 # Create your models here.
 
-class Photos(models.Mode):
-    photo = models.ImageField(upload_to='images/')
-    name = models.CharField(max_length=60)
-    description = models.TextField()
-    author = models.CharField(max_length=40, default='admin')
-    date = models.DateTimeField(auto_now_add=True)
-    type = models.ForeignKey(Type)
-    location = models.ForeignKey(Location)
-
-    @classmethod
-    def get_photo_by_id(cls,id):
-        photo = cls.objects.filter(id = id).all()
-        return photo
-
-    @classmethod 
-    def filter_by_location(cls,location):
-        photo_location = Photos.objects.filter(location__name = location).all()
-        return photo_location
-
-    @classmethod
-    def update_photo(cls,id,value):
-        cls.objects.filter(id = id).update(photo = value)
-
-    @classmethod
-    def search_by_type(cls, type):
-        photo = cls.objects.filter(type__name__icontains=type)
-        return photo
-
-    def __str__(self):
-        return self.name
-
-    def save_photo(self):
-        self.save()
-
-    def delete_photo(self):
-        self.delete()
-
-    class Meta:
-        ordering = ['date']
-
-class Type(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
 
-    def save_type(self):
+    def save_category(self):
         self.save()
 
     def delete_category(self):
         self.delete()
+
 
 class Location(models.Model):
     name = models.CharField(max_length=60)
@@ -73,4 +34,45 @@ class Location(models.Model):
         self.save()
 
     def delete_location(self):
-        self.delete() 
+        self.delete()
+
+
+class Photo(models.Model):
+    photo = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=60)
+    description = models.TextField()
+    author = models.CharField(max_length=40, default='admin')
+    date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category)
+    location = models.ForeignKey(Location)
+
+    @classmethod
+    def filter_by_location(cls, location):
+        photo_location = Photo.objects.filter(location__name=location).all()
+        return photo_location
+
+    @classmethod
+    def update_photo(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+    @classmethod
+    def get_photo_by_id(cls, id):
+        image = cls.objects.filter(id=id).all()
+        return image
+
+    @classmethod
+    def search_by_category(cls, category):
+        photos = cls.objects.filter(category__name__icontains=category)
+        return photos
+
+    def __str__(self):
+        return self.name
+
+    def save_photo(self):
+        self.save()
+
+    def delete_photo(self):
+        self.delete()
+
+    class Meta:
+        ordering = ['date']
