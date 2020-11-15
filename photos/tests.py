1a519b42d4c3ad6e8b8eda3a19c0b94114a894f9
Munge Ryan <mungeryan@gmail.com>
2020-11-15 17:02:12 +0300

    creating a testcase for class location

--- photos/tests.py
+++ photos/tests.py
@@ -66,3 +66,32 @@ class CategoryTestClass(TestCase):
         self.category.delete_category()
         category = Category.objects.all()
         self.assertTrue(len(category) == 0)
+
+class TestLocation(TestCase):
+    def setUp(self):
+        self.location = Location(name='Strath')
+        self.location.save_location()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location, Location))
+
+    def test_save_location(self):
+        self.location.save_location()
+        locations = Location.get_locations()
+        self.assertTrue(len(locations) > 0)
+
+    def test_get_locations(self):
+        self.location.save_location()
+        locations = Location.get_locations()
+        self.assertTrue(len(locations) > 1)
+
+    def test_update_location(self):
+        new_location = 'Mushroom'
+        self.location.update_location(self.location.id, new_location)
+        changed_location = Location.objects.filter(name='Mushroom')
+        self.assertTrue(len(changed_location) > 0)
+
+    def test_delete_location(self):
+        self.location.delete_location()
+        location = Location.objects.all()
+        self.assertTrue(len(location) == 0)
